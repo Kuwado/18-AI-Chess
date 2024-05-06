88cc11d4ccc71adc8a418677c76e2250233331d8
GNLimerence <phamhoanghainam203@gmail.com>
2024-05-06 19:37:04 +0700

    HN add hightlight+endgame

--- src/main.py
+++ src/main.py
@@ -53,53 +53,85 @@ def main():
     moved = False
     loadImages()  
     running = True
+    animate = False
+    game_over = False
+    move_log_font = pg.font.SysFont("Arial", 14, False, False)
     selected_piece = ()     # Lưu vị trí của quân cờ đang được chọn
-    player_click = []       # Lưu vị trí trước và sau
+    player_click = []    # Lưu vị trí trước và sau
     
     while running:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
             elif event.type == pg.MOUSEBUTTONDOWN:  # Xử lý sự kiện khi chuột được nhấn
-                location = pg.mouse.get_pos()
-                clicked_row = location[1] // PIECE_HEIGHT
-                clicked_col = location[0] // PIECE_WIDTH
-                if selected_piece == (clicked_row, clicked_col):  # Nếu click 1 quân cờ 2 lần
-                    selected_piece = ()
-                    player_click = []
-                else:  # Nếu chưa có quân cờ được chọn
-                    selected_piece = (clicked_row, clicked_col)
-                    player_click.append(selected_piece)
-                if len(player_click) == 2:
-                    move = State.Move(player_click[0], player_click[1], board)
-                    print(move.getChessNote())
-                    for i in range(len(val_move)):
-                        if move == val_move[i]:
-                            state.make_move(val_move[i])
-                            moved = True
-                            selected_piece = ()
-                            player_click = []
-                    if not moved:
-                        player_click = [selected_piece]
+                if not game_over:
+                    location = pg.mouse.get_pos()
+                    clicked_row = location[1] // PIECE_HEIGHT
+                    clicked_col = location[0] // PIECE_WIDTH
+                    if selected_piece == (clicked_row, clicked_col):  # Nếu click 1 quân cờ 2 lần
+                        selected_piece = ()
+                        player_click = []
+                    else:  # Nếu chưa có quân cờ được chọn
+                        selected_piece = (clicked_row, clicked_col)
+                        player_click.append(selected_piece)
+                    if len(player_click) == 2:
+                        move = State.Move(player_click[0], player_click[1], board)
+                        print(move.getChessNote())
+                        for i in range(len(val_move)):
+                            if move == val_move[i]:
+                                state.make_move(val_move[i])
+                                moved = True
+                                animate = True
+                                selected_piece = ()
+                                player_click = []
+                        if not moved:
+                            player_click = [selected_piece]
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_z:
                     state.remake_move()
                     moved = True
+                    animate = False
+                    game_over = False
         if moved:
-           val_move = state.validMove()
-           #print(val_move)
-           moved = False
+            if animate:
+                animateMove(state.moveLog[-1], screen, state.board, clock)
+            val_move = state.validMove()
+             #print(val_move)
+            moved = False
+            animate = False
 
-        drawGameState(screen, board)
+        drawGameState(screen, board, state, val_move, selected_piece)
+        if state.checkMate:
+            game_over = True
+            if state.turn:
+                drawEndGameText(screen, "Black wins by checkmate")
+            else:
+                drawEndGameText(screen, "White wins by checkmate")
+
+        elif state.staleMate:
+            game_over = True
+            drawEndGameText(screen, "Stalemate")
         clock.tick(15)
         pg.display.flip()  # Cập nhật màn hình
 
+#Highlight
+def highlightsq(screen, state, val_move, selected_piece):
+    if selected_piece != ():
+        row, col = selected_piece
+        if state.board[row][col][0] == ('w' if state.turn else 'b'):
+            s = pg.Surface((PIECE_WIDTH, PIECE_HEIGHT))
+            s.set_alpha(100)
+            s.fill(pg.Color('blue'))
+            screen.blit(s, (col*PIECE_WIDTH, row*PIECE_HEIGHT))
+            s.fill(pg.Color('yellow'))
+            for move in val_move:
+                if move.startRow == row and move.startCol == col:
+                    screen.blit(s, (PIECE_WIDTH*move.endCol, PIECE_HEIGHT*move.endRow))
 # Hàm khởi tạo sàn đấu
-def drawGameState(screen, board):
+def drawGameState(screen, board, state, val_move, selected_piece):
     drawBoard(screen)
+    highlightsq(screen, state, val_move, selected_piece)
     drawChessPieces(screen, board)
-    #highlight(screen, board, pos)
-
 
 # Hàm vẽ bàn cờ
 def drawBoard(screen):
@@ -121,35 +153,37 @@ def drawChessPieces(screen, board):
             if piece != "--":
                 screen.blit(images[piece], pg.Rect(column * PIECE_WIDTH, row * PIECE_HEIGHT, PIECE_WIDTH, PIECE_HEIGHT))
 
-# Hàm tô những ô quân cờ có thể đi được
-def highlight(screen, board, pos):
-    moves = []
-    if len(pos) == 2:
-        if board[pos[0]][pos[1]][1] == 'p':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        if board[pos[0]][pos[1]][1] == 'n':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        if board[pos[0]][pos[1]][1] == 'r':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        if board[pos[0]][pos[1]][1] == 'b':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        if board[pos[0]][pos[1]][1] == 'q':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        if board[pos[0]][pos[1]][1] == 'k':
-            rule.getPMove(board, pos[0], pos[1], moves)
-        anphaBG(screen, hlColorMain, pos)
-        for move in moves:
-            anphaBG(screen, hlColor, move)
-
-# Hàm tô background mờ
-def anphaBG(screen, color, pos):
-    hl_rect = pg.Rect(pos[1] * PIECE_WIDTH, pos[0] * PIECE_HEIGHT, PIECE_WIDTH, PIECE_HEIGHT)
-    hl_surface = pg.Surface((PIECE_WIDTH, PIECE_HEIGHT))
-    hl_surface.set_alpha(100)  # Đặt độ trong suốt
-    pg.draw.rect(hl_surface, color, hl_surface.get_rect())  # Vẽ hình chữ nhật trong suốt
-    screen.blit(hl_surface, hl_rect)
+def drawEndGameText(screen, text):
+    font = pg.font.SysFont("Helvetica", 32, True, False)
+    text_object = font.render(text, False, pg.Color("gray"))
+    text_location = pg.Rect(0, 0, MAX_WIDTH, MAX_HEIGHT).move(MAX_WIDTH / 2 - text_object.get_width() / 2,
+                                                                 MAX_HEIGHT / 2 - text_object.get_height() / 2)
+    screen.blit(text_object, text_location)
+    text_object = font.render(text, False, pg.Color('black'))
+    screen.blit(text_object, text_location.move(2, 2))
 
 
+
+def animateMove(move, screen, board, clock):
+    global colors
+    colors = [pg.Color(whiteChess), pg.Color(blackChess)]
+    coords = []
+    dR = move.endRow - move.startRow
+    dC = move.endCol - move.startCol
+    framesPerSq = 10
+    frameCount = (abs(dR) + abs(dC))*framesPerSq
+    for frame in range(frameCount+1):
+        row, col = (move.startRow + dR*frame/frameCount, move.startCol + dC*frame/frameCount)
+        drawBoard(screen)
+        drawChessPieces(screen, board)
+        color = colors[(move.endRow + move.endCol) % 2]
+        endSq = pg.Rect(move.endCol*PIECE_WIDTH, move.endRow*PIECE_HEIGHT, PIECE_WIDTH, PIECE_HEIGHT)
+        pg.draw.rect(screen, color, endSq)
+        if move.piece_cap != '--':
+            screen.blit(images[move.piece_cap], endSq)
+        screen.blit(images[move.piece_move], pg.Rect(col*PIECE_WIDTH, row*PIECE_HEIGHT, PIECE_WIDTH, PIECE_HEIGHT))
+        pg.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
